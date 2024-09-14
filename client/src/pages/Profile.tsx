9a2d64e3bd9afebad4006d40e06771138e64ff21
Peter Junior Mendy <petermendy5@gmail.com>
2024-09-14 05:57:28 +0100

    create profile page ui

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -8,7 +8,46 @@ const Profile = () => {
     return <Navigate to={"/login"} />;
   }
 
-  return <div>Profile</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col space-y-4">
+        <img
+          src={user?.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          value={user?.username}
+        />
+        <input
+          disabled
+          type="text"
+          placeholder="email"
+          className="border bg-slate-100 p-3 rounded-lg"
+          id="email"
+          value={user?.email}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
