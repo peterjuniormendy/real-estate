13dba1730c86e1b880ffa25ea3d29a0b7ef4ea07
Peter Junior Mendy <petermendy5@gmail.com>
2024-09-14 06:52:15 +0100

    uncomplete profile image upload functionality

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,8 +1,20 @@
+import { useEffect, useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
+import getStorage from "redux-persist/es/storage/getStorage";
 import { useAppSelector } from "../redux/hooks";
 
 const Profile = () => {
   const { user } = useAppSelector((state) => state.user);
+  const [file, setFile] = useState<File | undefined>(undefined);
+  console.log("profile", file);
+
+  const profileRef = useRef(null);
+
+  useEffect(() => {}, [file]);
+
+  const handleFileUpload = (file) => {
+    console.log(file);
+  };
 
   if (!user) {
     return <Navigate to={"/login"} />;
@@ -12,7 +24,15 @@ const Profile = () => {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col space-y-4">
+        <input
+          onChange={(e) => setFile(e.target.files?.[0])}
+          type="file"
+          ref={profileRef}
+          hidden
+          accept="image/*"
+        />
         <img
+          onClick={() => profileRef.current.click()}
           src={user?.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
